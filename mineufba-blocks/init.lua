55a5b3286a2a69bc96bba08f23d5af4517c1f2d5
Pedro Pontes <pontescpedro@gmail.com>
2018-11-18 20:39:51 -0300

    Update pilar block

--- mineufba-blocks/init.lua
+++ mineufba-blocks/init.lua
@@ -2,23 +2,23 @@ modName = "mineufba_blocks"
 
 -- PILAR COM FAIXA PRETA
 ------------------------------------------------------------------
+minetest.register_alias("pilar", modName .. ":pilar")
 
---[[
 minetest.register_node(modName .. ":pilar", {
-    description = "Pilar com faixa preta",
+    description = "Pilar",
     
     tiles = {modName .. "_pilar02.png",
-    		 modName .. "_pilar02.png",
-    		 modName .. "_pilar01.png",
-    		 modName .. "_pilar01.png",
-    		 modName .. "_pilar01.png",
-    		 modName .. "_pilar01.png"},
+             modName .. "_pilar02.png",
+             modName .. "_pilar01.png",
+             modName .. "_pilar01.png",
+             modName .. "_pilar01.png",
+             modName .. "_pilar01.png"},
 
     groups = {cracky = 3}
 
 })
-]]--
 
+--[[
 minetest.register_node(modName .. ":pilar", {
     description = "Pilar com faixa preta",
     drawtype = "mesh",
@@ -28,8 +28,8 @@ minetest.register_node(modName .. ":pilar", {
     groups = {cracky = 3}
 
 })
+]]--
 
-minetest.register_alias("pilar", modName .. ":pilar")
 
 
 ------------------------------------------------------------------
