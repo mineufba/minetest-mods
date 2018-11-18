81432a57e7ec6bc4086f42a2aa7343412dc27628
Pedro Pontes <pontescpedro@gmail.com>
2018-11-18 19:47:52 -0300

    Update blocks mod and add mtpaint fork

--- mineufba-blocks/init.lua
+++ mineufba-blocks/init.lua
@@ -3,6 +3,7 @@ modName = "mineufba_blocks"
 -- PILAR COM FAIXA PRETA
 ------------------------------------------------------------------
 
+--[[
 minetest.register_node(modName .. ":pilar", {
     description = "Pilar com faixa preta",
     
@@ -16,7 +17,19 @@ minetest.register_node(modName .. ":pilar", {
     groups = {cracky = 3}
 
 })
+]]--
+
+minetest.register_node(modName .. ":pilar", {
+    description = "Pilar com faixa preta",
+    drawtype = "mesh",
+    visual_scale = 1/16,
+    tiles = {modName .. "_pilar02.png"},
+    mesh = modName .. "_pilar_full.obj",
+    groups = {cracky = 3}
+
+})
 
 minetest.register_alias("pilar", modName .. ":pilar")
+
 
 ------------------------------------------------------------------
