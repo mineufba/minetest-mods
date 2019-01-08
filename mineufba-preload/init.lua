400b99efeb54dbb520dd7f587bc3c70c27178e40
Pedro Pontes <pontescpedro@gmail.com>
2019-01-07 22:09:07 -0300

    Update spawn position

--- mineufba-preload/init.lua
+++ mineufba-preload/init.lua
@@ -1,6 +1,6 @@
 minetest.register_on_newplayer(function(player) 
 
-		local pos = {x = 2865.5, y = -39.5, z = -1319.6}
+		local pos = {x = 3010.0, y = -31,0, z = -2360.0}
 		local name = player:get_player_name()
 
 		local privs = minetest.get_player_privs(name)
