4e40594579d4034e42ad5198929fc9b15385615d
Pedro Pontes <pontescpedro@gmail.com>
2019-05-04 13:56:20 -0300

    Remove itemstack reset method
    Remove method that changes the stack of items to 1 every time a item is used.

--- mineufba-preload/init.lua
+++ mineufba-preload/init.lua
@@ -34,29 +34,29 @@ minetest.register_on_respawnplayer(function(player)
 
 	end)
 
-minetest.register_on_dignode(function(pos, oldnode, digger)
+-- minetest.register_on_dignode(function(pos, oldnode, digger)
 
-		local inv = digger:get_inventory()
-		local listName = digger:get_wield_list()
+-- 		local inv = digger:get_inventory()
+-- 		local listName = digger:get_wield_list()
 
-		local searchStack = {name = oldnode.name, count = 2}
+-- 		local searchStack = {name = oldnode.name, count = 2}
 
-		if (inv:contains_item(listName, searchStack)) then 
+-- 		if (inv:contains_item(listName, searchStack)) then 
 
-			inv:remove_item(listName, {name = oldnode.name, count = 1})
+-- 			inv:remove_item(listName, {name = oldnode.name, count = 1})
 
-		end
+-- 		end
 
-	end)
+-- 	end)
 
-minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+-- minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
 
-		if (itemstack ~= nil) then
+-- 		if (itemstack ~= nil) then
 			
-			itemstack:set_count(1)
+-- 			itemstack:set_count(1)
 
-		end
+-- 		end
 
-		return true
+-- 		return true
 
-	end)
+-- 	end)
